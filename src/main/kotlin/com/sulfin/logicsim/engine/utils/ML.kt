63883a51ca05e9cdbf92bdd9be87bc4053924761
sulfin <40837857+sulfin@users.noreply.github.com>
2022-02-22 18:57:10 +0100

    added events

--- src/main/kotlin/com/sulfin/logicsim/engine/utils/ML.kt
+++ src/main/kotlin/com/sulfin/logicsim/engine/utils/ML.kt
@@ -1,13 +1,24 @@
 package com.sulfin.logicsim.engine.utils
 
 import com.sulfin.logicsim.engine.math.Vector2
+import org.apache.logging.log4j.kotlin.Logging
+import org.apache.logging.log4j.kotlin.logger
 import java.awt.event.MouseAdapter
 import java.awt.event.MouseEvent
 
 class ML : MouseAdapter() {
+    companion object : Logging
+
+    private val buttonPressed: MutableSet<Int> = HashSet()
+
+
+    private val pressCallbacks: MutableMap<Int, MutableList<(MouseEvent) -> Unit>> = HashMap()
+    private val releaseCallbacks: MutableMap<Int, MutableList<(MouseEvent) -> Unit>> = HashMap()
+    private val moveCallbacks: MutableMap<Int, MutableList<(MouseEvent) -> Unit>> = HashMap()
+    private val dragCallbacks: MutableMap<Int, MutableList<(MouseEvent) -> Unit>> = HashMap()
+
     var mousePressed = false
     var mouseDragged = false
-    var mouseButton = -1
     var pos = Vector2(-1f, -1f)
     var dpos = Vector2(-1f, -1f)
 
@@ -16,13 +27,21 @@ class ML : MouseAdapter() {
 
     override fun mousePressed(e: MouseEvent) {
         mousePressed = true
-        mouseButton = e.button
+        if (buttonPressed.add(e.button)) {
+//            logger.debug("pressing ${e.button}")
+            pressCallbacks[e.button]?.forEach { it(e) }
+        }
     }
 
     override fun mouseReleased(e: MouseEvent) {
         mousePressed = false
         mouseDragged = false
         dpos = Vector2()
+
+        if (buttonPressed.remove(e.button)) {
+//            logger.debug("release ${e.button}")
+            releaseCallbacks[e.button]?.forEach { it(e) }
+        }
     }
 
     override fun mouseMoved(e: MouseEvent) {
@@ -30,6 +49,7 @@ class ML : MouseAdapter() {
             e.x.toFloat(),
             e.y.toFloat()
         )
+        moveCallbacks[e.modifiersEx]?.forEach { it(e) }
     }
 
     override fun mouseDragged(e: MouseEvent) {
@@ -38,6 +58,27 @@ class ML : MouseAdapter() {
             e.x - pos.x,
             e.y - pos.y,
         )
+        dragCallbacks.filter { it.key and e.modifiersEx == it.key }.forEach { it.value.forEach { it(e) } }
+    }
+
+    fun registerMousePressed(button: Int, callback: (MouseEvent) -> Unit) {
+        if (pressCallbacks[button] == null) pressCallbacks[button] = ArrayList()
+        pressCallbacks[button]?.add(callback)
+    }
+
+    fun registerMouseReleased(button: Int, callback: (MouseEvent) -> Unit) {
+        if (releaseCallbacks[button] == null) releaseCallbacks[button] = ArrayList()
+        releaseCallbacks[button]?.add(callback)
+    }
+
+    fun registerMouseMoved(mask: Int, callback: (MouseEvent) -> Unit) {
+        if (moveCallbacks[mask] == null) moveCallbacks[mask] = ArrayList()
+        moveCallbacks[mask]?.add(callback)
+    }
+
+    fun registerMouseDragged(mask: Int, callback: (MouseEvent) -> Unit) {
+        if (dragCallbacks[mask] == null) dragCallbacks[mask] = ArrayList()
+        dragCallbacks[mask]?.add(callback)
     }
 
 }
